e71f42bbf1e90a1d81a7b819439e21ab54ade60a
Vladislav <vladprofmet@gmail.com>
2020-03-07 15:52:16 +0200

    Video add

--- main.js
+++ main.js
@@ -2,46 +2,28 @@ var owl=$(".owl-carousel")
 
 owl.owlCarousel({
     loop: true,
-    // autoHeight: false,
-    // autoWidth:  false,
+    autoHeight: false,
+    autoWidth: false,
     items: 1,
-    margin:10,
-    nav: false,
-    autoplay:true,
-    slideBy : 1,
-    autoHeight: true,
-    // mouseDrag: false,
+    margin: 0,
+    nav: true,
+    fluidSpeed: 100,
     autoplayTimeout: 15000, /* change to 5000 for normal autoplayTimeout. I set 1s only speed up this exemple */
-    autoplaySpeed: 1400,
-    dots: false,
-    mouseDrag: false,
-    touchDrag: false,
-    // onChanged: callBack
+    autoplaySpeed: 400,
+    navSpeed: 400,
+    onChanged: callBack
 });
-$(".double").on("touchstart mousedown", function(e) {
-    // Prevent carousel swipe
-    e.stopPropagation();
-})
-//  function callBack(event) {
-//  	// Solution to have correct item number. See: https://github.com/OwlCarousel2/OwlCarousel2/issues/1029. Thanks to Modicrumb
-//  	var current = (event.item.index + 1) - event.relatedTarget._clones.length / 2;
-//    	var allItems = event.item.count;
-//    	if (current > allItems || current <= 0) {
 
-//      	current = allItems - (current % allItems);
+function callBack(event) {
+    // Solution to have correct item number. See: https://github.com/OwlCarousel2/OwlCarousel2/issues/1029. Thanks to Modicrumb
+    var current = (event.item.index + 1) - event.relatedTarget._clones.length / 2;
+    var allItems = event.item.count;
+    if (current > allItems || current <= 0) {
+        current = allItems - (current % allItems);
+    }
 
-//    	}
-
-//    	if(current==1){
-
-//    		$('video').attr('autoplay',true);
-//    		owl.trigger('stop.owl.autoplay')
-// setTimeout(function() {owl.trigger('play.owl.autoplay')}, 93000)
-//    	}
-//    	else{
-//    		$("video").removeAttr("autoplay",true)
-
-//    	}
-//  }
-
-
+    if(current==1){
+        owl.trigger('stop.owl.autoplay')
+        setTimeout(function() {owl.trigger('play.owl.autoplay')}, 93000)
+    }
+}
