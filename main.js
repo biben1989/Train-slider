5b3621afad2bea6e3e67cbe1ee8f1de12685429e
Vladislav <vladprofmet@gmail.com>
2020-03-07 20:22:48 +0200

    video delet
    comit

--- main.js
+++ main.js
@@ -5,25 +5,27 @@ owl.owlCarousel({
     autoHeight: false,
     autoWidth: false,
     items: 1,
-    margin: 0,
     nav: true,
+    mouseDrag:false,
+    margin: 10,
+
     fluidSpeed: 100,
     autoplayTimeout: 15000, /* change to 5000 for normal autoplayTimeout. I set 1s only speed up this exemple */
     autoplaySpeed: 400,
     navSpeed: 400,
-    onChanged: callBack
+    // onChanged: callBack
 });
 
-function callBack(event) {
-    // Solution to have correct item number. See: https://github.com/OwlCarousel2/OwlCarousel2/issues/1029. Thanks to Modicrumb
-    var current = (event.item.index + 1) - event.relatedTarget._clones.length / 2;
-    var allItems = event.item.count;
-    if (current > allItems || current <= 0) {
-        current = allItems - (current % allItems);
-    }
-
-    if(current==1){
-        owl.trigger('stop.owl.autoplay')
-        setTimeout(function() {owl.trigger('play.owl.autoplay')}, 93000)
-    }
-}
+// function callBack(event) {
+//     // Solution to have correct item number. See: https://github.com/OwlCarousel2/OwlCarousel2/issues/1029. Thanks to Modicrumb
+//     var current = (event.item.index + 1) - event.relatedTarget._clones.length / 2;
+//     var allItems = event.item.count;
+//     if (current > allItems || current <= 0) {
+//         current = allItems - (current % allItems);
+//     }
+//
+//     if(current==1){
+//         owl.trigger('stop.owl.autoplay')
+//         setTimeout(function() {owl.trigger('play.owl.autoplay')}, 93000)
+//     }
+// }
